f3a976aa27a91fe9b6507958b941156d306f9469
Konstantinos Karatsenidis <gate2k1@gmail.com>
2018-05-15 03:02:58 +0300

    int queue

---
+++ queue.h
@@ -0,0 +1,10 @@
+
+/*dhlwseis typwn*/
+
+typedef int QueueType;
+
+typedef struct 
+{   int embros;		/*8esh toy prwtoy stoixeoy ths oyras*/
+	int piso;		/*8esh toy teleytaioy stoixeioy ths oyras*/
+	QueueType* buffer;	/*o pinakas twn stoixeiwn*/
+} Queue;
